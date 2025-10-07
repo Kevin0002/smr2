c9d621b6c7a10c345801a71b0077d4e33eacb012
Kevin Cuellar Arias <kevin@MacBook-Pro-de-Kevin.local>
2025-10-07 15:17:14 +0200

    Primer subida smr2

---
+++ Python/ejercicio01.py
@@ -0,0 +1,10 @@
+print("Kevin\n") # Ejercicio 01
+print("Kevin\n","calle no importa 28\n",634273819) # Ejercicio 02
+print("    *") # Ejercicio 02
+print("   ***")
+print("  *****")
+print(" *******")
+print("*********")
+num = input("Ingrese el primer número")
+num1 = input("Ingrese el segundo número")
+operacion = input(Ingrese que operecion quiere realizar: +, -, *, /)
---
+++ Python/hola.py
@@ -0,0 +1,18 @@
+print("Hola Mundo") # La función print para imprimir en pantalla
+
+a=3 # Variable de tipo entero
+b=7
+resultado=a+b # Variable que hace la función de sumar dos variable de tipo entero y almacenar el resultado
+print(resultado)
+
+hola="Hola Mundo" 
+adios="Perras"
+espacio=" "
+resultado02=hola+espacio+adios
+print(resultado02)
+
+if 5 > 6:
+    print("Hola Mundo")
+else:
+    print("Adios Mundo")
+
