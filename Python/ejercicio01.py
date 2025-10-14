7fcb4f03ec9b60fc7e9817fab569b5ddbdfb31d4
Kevin Cuellar Arias <kevin@MacBook-Pro-de-Kevin.local>
2025-10-14 16:07:19 +0200

    Actualización archivo redes

--- Python/ejercicio01.py
+++ Python/ejercicio01.py
@@ -1,10 +1,37 @@
-print("Kevin\n") # Ejercicio 01
-print("Kevin\n","calle no importa 28\n",634273819) # Ejercicio 02
-print("    *") # Ejercicio 02
-print("   ***")
-print("  *****")
-print(" *******")
-print("*********")
-num = input("Ingrese el primer número")
-num1 = input("Ingrese el segundo número")
-operacion = input(Ingrese que operecion quiere realizar: +, -, *, /)
+# print("Kevin\n") # Ejercicio 01
+# print("Kevin\n","calle no importa 28\n",634273819) # Ejercicio 02
+# print("    *") # Ejercicio 03
+# print("   ***")
+# print("  *****")
+# print(" *******")
+# print("*********")
+# num = input("Ingrese el primer número: ")
+# num1 = input("Ingrese el segundo número: ")
+# operacion = input("Ingrese que operecion quiere realizar: +, -, *, /, **: ") # Ejercicio 04
+# if operacion == "+":
+#     print("El resultado es: ", int(num) + int(num1))
+# elif operacion == "-":
+#     print("El resultado es: ", int(num) - int(num1))
+# elif operacion == "*":
+#     print("El resultado es: ", int(num) * int(num1))
+# elif operacion == "/":
+#     print("El resultado es: ", int(num) / int(num1))
+# elif operacion == "**":
+#     print("El resultado es: ", int(num) ** int(num1))
+# else:
+#     print("Operación no válida") 
+
+# num = 1.3
+# print(f"{num:.20f}") # Ejercicio 05
+
+texto = "Hola Mundo que tal estan de locos sapo perro\n"
+print (type("perro" in texto))
+
+a = "Hola"
+b = "HOLA"
+
+print(a == b) # False, porque diferencia mayúsculas/minúsculas
+print(a.lower() == b.lower()) # True, porque ambas quedan "hola" antes de comparar
+
+print (texto * 3)
+
