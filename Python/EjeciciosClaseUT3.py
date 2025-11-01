1cd58e9c8f806b5c074331acbbfd112b4dd42ed3
Kevin <kevincuellararias994@gmail.com>
2025-11-01 02:39:08 +0100

    Update EjeciciosClaseUT3.py

--- Python/EjeciciosClaseUT3.py
+++ Python/EjeciciosClaseUT3.py
@@ -1,18 +1,18 @@
 """
-1) Mete los valores del 1 al 100 en una lista. 
+1) Mete los valores del 1 al 100 en una lista.
 a) Imprimirlos ordenados
 b) Imprimirlos al revés.
 c) Pide un número por teclado y comprueba si se encuentra en la lista. En caso de que si esté
 debe salir del bucle.
 """
 
-lista =list(range(1, 101))
+lista = list(range(1, 101))
 print(lista)
 
 print(lista[::-1])
 
 while True:
-    num=int(input("Introduce un numero y comprueba que este en la lista: "))
+    num = int(input("Introduce un numero y comprueba que este en la lista: "))
     if num in lista:
         print(f"El numero {num} esta en la lista")
         break
@@ -24,8 +24,19 @@ while True:
 si pide el 5 la lista tendrá: 5,10,15,20,25,30,35,40,45,50
 """
 
-num=int(input("Introduce un numero para ver su tabla de multiplicar: "))
-tablaMultiplicar = [num, num * 2, num * 3, num * 4, num * 5, num * 6, num * 7, num * 8, num * 9, num * 10]
+num = int(input("Introduce un numero para ver su tabla de multiplicar: "))
+tablaMultiplicar = [
+    num,
+    num * 2,
+    num * 3,
+    num * 4,
+    num * 5,
+    num * 6,
+    num * 7,
+    num * 8,
+    num * 9,
+    num * 10,
+]
 
 print("La tapla de multiplicar de {num} es: ", tablaMultiplicar)
 
@@ -38,10 +49,10 @@ lista = []
 i = 0
 while i < 10:
     valor = int(input("introduce un valor a la lista: "))
-    lista.append(valor)  
+    lista.append(valor)
     i += 1
 
-print("Lista completa: ",lista)
+print("Lista completa: ", lista)
 
 suma = sum(lista)
 media = suma / len(lista)
@@ -56,7 +67,21 @@ longitud máxima de la tupla, muestra el contenido de esa posición sino muestra
 El programa termina cuando el usuario introduce un cero. (while)
 """
 
-meses = ("ERROR", "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+meses = (
+    "ERROR",
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+)
 
 while True:
     i = int(input("Introduce un numero de un mes: "))
@@ -73,12 +98,28 @@ Usa el método sort para ordenar
 """
 lista = []
 
-while valor != 0:
+while True:
     valor = int(input("introduce un valor a la lista: "))
+    if valor == 0:
+        break
+
     lista.append(valor)
 
-print(f"Lista ordenada de menor a mayor: ", lista.sort)
+lista.sort
+print(f"Lista ordenada de menor a mayor: {lista}")
 
+"""
+6) Lo mismo que el anterior pero ordenando de mayor a menor.
+"""
 
+lista = []
+while True:
+    valor = int(input("introduce un valor a la lista: "))
+    if valor == 0:
+        break
 
+    lista.append(valor)
 
+lista.sort(reverse=True)
+
+print(f"Lista ordenara de mayor a menor: {lista}")
